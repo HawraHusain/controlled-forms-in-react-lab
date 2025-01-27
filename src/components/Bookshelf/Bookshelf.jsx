ecf163d7959d170ea3241e0f2970b2d1161292d4
HawraHusain <hawraanowrooz158@gmail.com>
2025-01-27 20:09:45 +0300

    setform

---
+++ src/components/Bookshelf/Bookshelf.jsx
@@ -0,0 +1,66 @@
+import { useState } from 'react';
+
+const App = () => {
+    let initialState ={
+        title:'',
+        author: '',
+    }
+    const [books, setBooks] = useState([
+        { title: 'Fourth Wing', author: 'Rebecca Yarros' },
+        { title: 'The Lion, the Witch and the Wardrobe', author: 'C.S. Lewis' },
+    ]);
+    const [newBook, setnewBooks] = useState({initialState})
+
+    const handleInputChange = (event) => {
+        const newBookData ={ ...newBook, [event.target.name]: event.target.value }
+        setnewBooks(newBookData)
+    }
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        setBooks(`Your new book is: ${newBook.title} ${newBook.author}`);
+        setnewBooks({ title: '', author: '' });
+    }
+    return (
+        <>
+            <div className="bookshelfDiv">
+                <div className="formDiv">
+                    <h3>Add a Book</h3>
+                    <form onSubmit={handleSubmit}>
+                        <label htmlFor="title">Title: </label>
+                        <input
+                            id="title"
+                            name="title"
+                            type="text"
+                            value={newBook.title}
+                            onChange={handleInputChange}
+                        />
+                        <label htmlFor="author">Author: </label>
+                        <input
+                            id="author"
+                            name="author"
+                            type="text"
+                            value={newBook.author}
+                            onChange={handleInputChange}
+                        />
+                        
+                        <button type="submit">Submit</button>
+                      
+                    </form>
+                </div>
+                <div className="bookCardsDiv">
+                    <ul >
+                        {books.map((book) =>
+                            <li className='bookCard'>
+                                <h3>{book.title}</h3>
+                                <p>{book.author}</p>
+                            </li>
+                        )}
+                    </ul>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default App;
+
